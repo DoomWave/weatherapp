92fefc2ba19be64ed3208018ed9e641af799628f
DoomWave <acunanajair9@gmail.com>
2024-09-11 15:15:55 -0700

    CurrentWeather added, and rebuilding the Index.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,65 +1,17 @@
-import { Image, StyleSheet, Platform, View, Text, SafeAreaView  } from 'react-native';
-import React from 'react';
-import { Icon } from '@expo/vector-icons/build/createIconSet';
-import { Feather } from '@expo/vector-icons';
+import React  from "react"
+import { StyleSheet, View } from "react-native"
+import CurrentWeather from "@/components/CurrentWeather"
 
 const App = () => {
-  return(
-   <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Feather name="sun" size={100} color="black" />
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8 </Text>
-          <Text style={styles.highLow}>low: 5</Text>
-        </View>
-      </View>
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>Its Sunny</Text>
-        <Text style={styles.message}>Its Perfect t-shirt weather</Text>
-      </View>
-    </SafeAreaView>
-  ) 
+  return (
+    <View style={styles.container}> 
+
+    </View>
+  )
 }
-
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    backgroundColor: 'pink',
-  },
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  temp: {
-      color: 'black',
-      fontSize: 48
-  },
-  feels: {
-    fontSize: 30,
-    color: 'black'
-  },
-  highLow: {
-    color: 'black',
-    fontSize: 20
-  },
-  highLowWrapper: {
-    flexDirection: 'row',
-  },
-  bodyWrapper: {
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start',
-    paddingLeft: 25,
-    marginBottom: 40
-  },
-  description: {
-    fontSize: 48
-  },
-  message: {
-    fontSize: 30
-  },
-
+    flex: 1
+  }
 })
 export default App
